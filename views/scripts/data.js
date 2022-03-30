3587e8ce8a4bc7c8899a5d017471cd620e9fea36
Suraj Devere <95920234+Surajdevere09@users.noreply.github.com>
2022-03-30 19:19:50 +0530

    grocery heroku api added

--- views/scripts/data.js
+++ views/scripts/data.js
@@ -53,7 +53,7 @@ async function  grocery(){
 
 
   try{
-      let res = await fetch("https://blinkit-clone-backend.herokuapp.com/grocery/")
+      let res = await fetch("https://blinkit-clone-backend.herokuapp.com/grocery")
 
       let data = await res.json()
       
